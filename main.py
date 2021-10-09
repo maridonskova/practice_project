ab7d2601710d61ed0e604fd12c4f7b7b32837f32
Donskova Mariya <dons0306@yandex.ru>
2021-10-09 17:34:48 +0300

    add main.py

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import mean_absolute_error
+from sklearn.preprocessing import PolynomialFeatures
+
 import numpy as np
+
 from module_a import polinom_3
 from module_b import hyperbola
-X = np.random.uniform(low=0.0, high=1.0, size=1000)
-if np.random.binominal(1, 0.5): 
+
+X = np.array(range(-1000, 1000))
+trans = PolynomialFeatures(degree=3)
+data = trans.fit_transform(X)
+
+print('Print type polinom or hyperbola')
+type_f = input()
+
+if type_f == 'polinom': 
     Y = polinom_3(X)
-else:
+elif type_f == 'hyperbola':
     Y = hyperbola(X)
-clf = RandomForestClassifier(max_depth=2, random_state=0)
-clf.fit(X, y)
-print((clf.predict(input())))
+else:
+    print('error type')
+
+clf = RandomForestClassifier(max_depth=4, random_state=0)
+clf.fit(data[:-250], Y[:-250])
+pred_ = clf.predict(data[-250:])
+print(f'MAE is {mean_absolute_error(pred_, Y[-250:])}')
+
